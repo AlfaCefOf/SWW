85bc4c5a46c5d7d3ef39a5bbc2fd48c057ed3934
AlfaCefOf <alfaxcef@gmail.com>
2025-08-04 15:38:13 +0500

    edited error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/home";
-import Games from "./pages/Games/games";
 import Movies from "./pages/Movies/movies";
 import GameDetails from "./pages/GameDetails/gamedetails";
 import MovieDetails from "./pages/MovieDetails/moviedetails";
 import Header from "./Components/Header/header";
 import "./App.css";
+import Home from "./Pages/Home/home";
+import Games from "./Pages/Games/games";
 
 function App() {
   return (
